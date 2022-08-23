5ca9ef6b148300c855bd7ddfcb7132e560b62e1f
joel ibaceta <mail@joelibaceta.com>
2022-08-23 11:21:35 +0900

    updating program

--- src/component/calendar.tsx
+++ src/component/calendar.tsx
@@ -38,35 +38,38 @@ export default function Calendar() {
       </aside>
       <aside className="classHours">
         <article className="hours">
-          <div className="item">30 min</div>
-          <div className="item">60 min</div>
-          <div className="item">90 min</div>
-          <div className="item">120 min</div>
+          <div className="item">10:00 - 10:30</div>
+          <div className="item">10:30 - 10:50</div>
+          <div className="item">12:00 - 12:30</div>
+          <div className="item">12:30 - 13:00</div>
         </article>
         <article className="courses">
           <div className="week-days">
             <div className="dayOne">
               <div className="item-1">
-                Bienvenida. Introducción y repaso Python
+                Bienvenida y repaso de conceptos basicos de programación
               </div>
               <div className="item-1">
-                El interprete de Python, CPython y entornos visuales
+                ¿Que es el codigo idiomatico?, el Zen de Python y PEP8
               </div>
-              <div className="item-2">
-                Sintáxis Python. Tipos de datos, Operadores.
+              <div className="item-1">
+                Compiladores, Interpretes y el Interprete de Python
+              </div>
+              <div className="item-1">
+                Ejecutando codigo e Introduccion a la sintaxis del lenguaje
               </div>
             </div>
             <div className="dayOne">
-              <div className="item-1">Programación estructurada</div>
-              <div className="item-2 highlights">Ejercicios</div>
-              <div className="item-1">Manejo de errores</div>
+              <div className="item-1">Programacion estructurada, Variables y Estructuras de control</div>
+              <div className="item-1 highlights">Tipos de datos y Operadores</div>
+              <div className="item-1">Introduccion a las estructuras de datos, Listas, Tuplas y Diccionarios</div>
+              <div className="item-1">Operaciones con estructuras de datos</div>
             </div>
           </div>
           <div className="week-days">
             <div className="dayOne">
-              <div className="item-1">Listas, tuplas y colecciones</div>
-              <div className="item-1">Algoritmos y estructura de datos</div>
-              <div className="item-2 highlights">Ejercicios</div>
+              <div className="item-2">Estructuras de datos avanzadas (Colas, Pilas, Sets, Arboles, Listas enlazadas, etc) </div>
+              <div className="item-2 highlights">Algoritmos utiles con estructuras de datos</div>
             </div>
             <div className="dayOne">
               <div className="item-2 highlights">
