1b97ba9fa5bed0c8e972f10bde2ab807a28c07a4
joel ibaceta <mail@joelibaceta.com>
2022-08-23 17:33:35 +0900

    remove unused requirements

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,7 @@
 import Header from "../component/shared/header";
 import Hero from "../component/hero";
-import FormContact from "../component/form";
 import Footer from "../component/shared/footer";
-import Modality from "../component/modality";
-import Brochure from "../component/brochure";
+import Modality from "../component/modality"; 
 import Calendar from "../component/calendar";
 
 export default function Home() {
