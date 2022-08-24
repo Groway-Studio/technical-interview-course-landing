5aa01a5a60b6efb79da2672f9d5f0ef9191f6c26
joel ibaceta <mail@joelibaceta.com>
2022-08-24 19:37:49 +0900

    update wording

--- src/component/instructor.tsx
+++ src/component/instructor.tsx
@@ -1,18 +1,27 @@
 import "../sass/component/_instructor.scss";
 
+import profile from "../assets/profile.png";
+
 export default function Instructor() {
   return (
     <section className="instructor">
-      <div className="instructor__profile-image"></div>
+      <div 
+        className="instructor__profile-image"
+        style={{
+          backgroundImage: `url(${profile})`,
+        }}
+      >
+      </div>
       <div className="instructor__profile-info">
-        <h2>Lorem Ipsum</h2>
+        <h2>Sobre el Instructor</h2>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe eius
-          excepturi ab ullam. Rem modi molestiae beatae consequuntur.
+          Joel Ibaceta es un programador experimentado con mas de 15 años de experiencia en la industria, ha trabajando en diversas empresas a lo largo de la latinoamerica, desde startups, corporaciones y unicornios. 
         </p>
         <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum
-          consequuntur enim possimus dolorum maiores nam dolores perspiciatis.
+          Actualmente es CTO de Kwema Inc. en los Estados Unidos, uno de sus ultimos emprendimientos donde lleva adelante proyectos sobre IOT para la seguridad laboral.
+        </p>
+        <p>
+          El nos acompañara desde Seul, Corea del Sur, y sera nuestro instructor en esta primera edicion de nuestro curso de Python.
         </p>
       </div>
     </section>
