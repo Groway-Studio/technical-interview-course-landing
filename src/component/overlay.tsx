e9e5f215ce856e0567bf68588df9dc4e86e31672
joel ibaceta <mail@joelibaceta.com>
2022-08-27 19:49:07 +0900

    overlay basic structure

---
+++ src/component/overlay.tsx
@@ -0,0 +1,12 @@
+export default function Overlay() {
+    return (
+        <div className="overlay">
+            <div className="overlay__content">
+                <div className="overlay__content__title">
+                    <h1>Pronto estara disponible la venta publico, ten paciencia</h1>
+                    <span>Por ahora solo se comprar usando un enlace de invitación</span>
+                </div>
+            </div>
+        </div>
+    );
+}
