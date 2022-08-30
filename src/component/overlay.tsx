ab6058217861f3a53a78ff7744250be4c2cdd5e6
cristiandelcidd <cristian_delcid10@yahoo.com>
2022-08-30 11:52:28 -0600

    Timeline added, and responsive button fixed

--- src/component/overlay.tsx
+++ src/component/overlay.tsx
@@ -1,20 +1,22 @@
-import wait from "../assets/wait.png";
+import coming_soon from "../assets/coming-soon/coming-soon.png";
 import "../sass/component/_overlay.scss";
 
 export default function Overlay() {
   return (
     <div className="overlay">
       <div className="overlay__content">
-        <div className="overlay__content__title">
-          <h1>Pronto estará disponible la venta pública, ten paciencia...</h1>
-          <span>Por ahora sólo se comprar usando un enlace de invitación.</span>
-        </div>
         <img
-          src={wait}
+          src={coming_soon}
           alt="Espera"
           className="overlay__content__image"
           draggable={false}
         />
+        <div className="overlay__content__title">
+          <p>Pronto estará disponible la venta pública</p>
+          <span>
+            Por ahora sólo se puede comprar usando un enlace de invitación.
+          </span>
+        </div>
       </div>
     </div>
   );
