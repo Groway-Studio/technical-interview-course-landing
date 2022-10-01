eef9027206c8711d468c60d40af9a3e4e43a0a0d
Joel Ibaceta <mail@joelibaceta.com>
2022-10-01 11:07:16 +0900

    Update useUserInfo.ts

--- src/hooks/useUserInfo.ts
+++ src/hooks/useUserInfo.ts
@@ -69,7 +69,7 @@ const useUserInfo = () => {
             user_email: email,
             user_phone: `+51${phoneNumber}`,
             invitation_code: getParameterByName("invitation_code"),
-            price: 150.0,
+            price: 75.0,
           }),
         });
 
